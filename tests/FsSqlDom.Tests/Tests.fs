049e7d12621682b13f6113d61f6bf202ff072f34
Isak Sky <isak.sky@gmail.com>
2016-11-17 19:23:21 -0700

    Initial commit

---
+++ tests/FsSqlDom.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module FsSqlDom.Tests
+
+open FsSqlDom
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
